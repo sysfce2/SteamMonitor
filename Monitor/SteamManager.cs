60d84d9cacf669bb6c2e09c6249a5f3d9f0695b2
Pavel Djundik <xPaw@users.noreply.github.com>
2020-10-22 19:36:13 +0300

    Update packages

--- Monitor/SteamManager.cs
+++ Monitor/SteamManager.cs
@@ -7,7 +7,7 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Dapper;
-using MySql.Data.MySqlClient;
+using MySqlConnector;
 using SteamKit2;
 using SteamKit2.Discovery;
 
